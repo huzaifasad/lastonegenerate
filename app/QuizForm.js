d426697ca08f3fd988342c87c8437a1478cbc5b4
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2024-07-23 13:06:32 -0700

    Update QuizForm.js

--- app/QuizForm.js
+++ app/QuizForm.js
@@ -361,7 +361,10 @@ export default function QuizForm() {
    <button
           type="submit"
           className="w-26 p-2 mb-5 bg-[#434343] text-[#FFFFFF] p-2 tracking-wide rounded flex items-center justify-center ml-auto"
- disabled={isLoading || !formData.recaptchaToken}         >
+ 
+              disabled={isLoading || !formData.recaptchaToken} // Disable button if loading or CAPTCHA not verified
+
+            >
           {isLoadingx ? (
             <div className="flex items-center justify-center">
               <svg
